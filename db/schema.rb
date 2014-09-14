33bac3328e1eed13572f18cddced7c7bffa116c2
Andy Perez <agperez@gmail.com>
2014-09-14 12:08:19 -0400

    Add Picture Change to non-Admin

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,10 @@
 ActiveRecord::Schema.define(version: 20140907103734) do
 
   create_table "email_contents", force: true do |t|
-    t.string   "email"
-    t.text     "text"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "origin"
-    t.string   "recipient"
+    t.string "email"
+    t.text   "text"
+    t.string "origin"
+    t.string "recipient"
   end
 
   create_table "events", force: true do |t|
