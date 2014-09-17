11b6ed5c2bb2ae9553bf86091f6a19c9b7aa5ca7
Andy Perez <agperez@gmail.com>
2014-09-17 14:26:56 -0400

    mail setup

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "localhost:3000",
   :user_name            => "creightonvaughn@gmail.com",
-  :password             => "",
+  :password             => "C200204960v",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
