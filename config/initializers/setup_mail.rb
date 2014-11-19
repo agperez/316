a73aaeda950082f257bc1fc40867e2e4bec8642e
Crighton Vaughn <creightonvaughn@gmail.com>
2014-11-19 14:41:05 -0500

    setlist regex

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
+  :address              => 'smtp.gmail.com',
   :port                 => 587,
-  :domain               => "localhost:3000",
-  :user_name            => "creightonvaughn@gmail.com",
-  :password             => "C200204960v",
-  :authentication       => "plain",
+  :domain               => 'gmail.com',
+  :user_name            => 'creightonvaughn@gmail.com',
+  :password             => 'C200204960v',
+  :authentication       => 'plain',
   :enable_starttls_auto => true
 }
