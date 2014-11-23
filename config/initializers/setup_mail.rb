6f8aac28bb8a2ddf792d8154e793d09784970bc9
Andy Perez <agperez@gmail.com>
2014-11-23 11:35:56 -0500

    removing setup_mail.rb

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "localhost:3000",
-  :user_name            => "creightonvaughn@gmail.com",
-  :password             => "",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
