ff707ef2a390623f719bc94af9184ffd1ed3cd76
Crighton Vaughn <creightonvaughn@gmail.com>
2014-12-18 15:18:44 -0500

    setlist

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,7 @@
 class Song < ActiveRecord::Base
   has_many :setlist_songs
   has_many :setlists, through: :setlist_songs
+
+  validates :name, presence: true,
+            uniqueness: { case_sensitive: false }
 end
