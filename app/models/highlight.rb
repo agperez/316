a91ef8a05430bbbda0b02b31641ac0c1887f39e0
Matthew Thompson <matt@mattathompson.org>
2015-01-30 17:27:30 -0500

    adding friendly_url

--- app/models/highlight.rb
+++ app/models/highlight.rb
@@ -1,5 +1,7 @@
 class Highlight < ActiveRecord::Base
   validates_presence_of :title, :created_at, :video, :audio, :sermon_link
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 
   self.per_page = 5
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129180455) do
+ActiveRecord::Schema.define(version: 20150130222426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,10 @@ ActiveRecord::Schema.define(version: 20150129180455) do
     t.string   "audio"
     t.datetime "created_at"
     t.string   "sermon_link"
+    t.string   "slug"
   end
+
+  add_index "highlights", ["slug"], name: "index_highlights_on_slug", using: :btree
 
   create_table "ministries", force: true do |t|
     t.string   "name"
