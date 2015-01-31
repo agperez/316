48abc755467e187c8e295f6612a9e2181beb1e6f
Matthew Thompson <matt@mattathompson.org>
2015-01-30 22:59:08 -0500

    creates user & highlights factories

--- spec/factories/highlight.rb
+++ spec/factories/highlight.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :highlight do
     title Faker::Company.name
-    video
-    audio
-    created_at
-    sermon_link
-
+    video Faker::Internet.url
+    audio Faker::Internet.url
+    created_at Date.today
+    sermon_link Faker::Internet.url
+    slug Faker::Internet.slug( title, '_')
   end
 end
---
+++ spec/factories/user.rb
@@ -0,0 +1,39 @@
+FactoryGirl.define do
+  factory :user do
+    first_name Faker::Name.first_name
+    email Faker::Internet.email
+    password Faker::Internet.password
+    last_name Faker::Name.last_name
+    gender ['male','female'].sample
+    birth_date Faker::Date.backward
+    spouse Faker::Name.name
+    address1 Faker::Address.street_address
+    city Faker::Address.city
+    state Faker::Address.state
+    zip Faker::Address.zip.to_i
+  end
+end
+
+trait :gets_reminders
+  reminders true
+end
+
+trait :admin do
+  admin true
+end
+
+trait :deactivated do
+  deactivated true
+end
+
+trait :hides_email do
+  hide_email true
+end
+
+trait :hides_phone do
+  hide_phone true
+end
+
+trait :hides_address do
+  hide_address true
+end
