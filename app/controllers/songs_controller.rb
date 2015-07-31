e0c8b64768ec299aa0f882e3f0095f49c4943e4c
Crighton Vaughn <creightonvaughn@gmail.com>
2015-07-31 15:12:31 -0400

    updates regex

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -70,8 +70,8 @@ class SongsController < ApplicationController
       chordregex = /([A-G][b\#]?(2|5|6|7|9|11|13|6\/9|7\-5|7\-9|7\#5|7\#9|7\+5|7\+9|7b5|7b9|7sus2|7sus4|add2|add4|add9|aug|dim|dim7|m\/maj7|m6|m7|m7b5|m9|m11|m13|maj7|maj9|maj11|maj13|mb5|m|sus|sus2|sus4|)*(\-)*)/
       splitlyrics.each_with_index do |index|
         if chordregex.match(index)
-          chords.push(index)
-          splitlyrics.delete(index)
+            chords.push(index)
+            splitlyrics.delete(index)
         end
       end
     @song.lyrics = splitlyrics.join
